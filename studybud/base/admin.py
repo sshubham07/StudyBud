1131db8c03f8815c2902f53df79f6b485af2c4c8
Shubham Kumar Gupta <shubhamkumargupta2@gmail.com>
2023-08-08 08:50:22 +0530

    Initial Commit

---
+++ studybud/base/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Room)
+admin.site.register(Message)
+admin.site.register(Topic)
+admin.site.register(User)
+
+
+
